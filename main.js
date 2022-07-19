714deac367954782bfc4bd854e782369cd8e451a
vijaydev133 <vijaydeveloper004@gmail.com>
2022-07-19 17:13:26 +0530

    Adding code

---
+++ main.js
@@ -0,0 +1,65 @@
+let students = [
+    {
+        img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUHK_xd4GHWvzXkA3DygGiU3---JdQdHXbA_uTguFvckcwsMDakFodlYlaQMv4p6fWM5I&usqp=CAU",
+        name : "Vijay",
+        desc : "Fresher from Tenkasi",
+        prmskills : "Html,Css,Javascript,React JS",
+        secskills: "Visual Studio Code, Git",
+        expsl : "3.5L" ,
+        edu : "B.Tech-EIE-2020-7.05 HSC - State Board - 2016 - 70%.SSLC - State Board - 2014 - 85%.",
+        avai : "Immediate",
+        isPlaced : false,
+        mbl : 9259874550,
+        batch : "FSD10",
+        git : "https://github.com/vijaydev133",
+        gitUName: "vijaydev133" ,
+        Refby : "M. Chermaraja",
+        loc : "Online",
+        plcsts : "Not Started"
+
+    },
+    {
+        img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSla-p7MqWVSLL2rpSQHlxEO6mKceKYPvZjo4oslefoeXE7-oMcRHP5IfT3qgllHC8kKvQ&usqp=CAU",
+        name : "Ajay",
+        desc : "Fresher from madurai",
+        prmskills : "Html,Css,Javascript,React JS",
+        secskills: "Visual Studio Code, Git",
+        expsl : "2.5L" ,
+        edu : "B.Tech-EIE-2020-7.05 HSC - State Board - 2016 - 70%.SSLC - State Board - 2014 - 85%.",
+        avai : "Immediate",
+        isPlaced : True,
+        mbl : 938527410,
+        batch : "FSD09",
+        git : "https://github.com/vijaydev133",
+        gitUName: "vijaydev133" ,
+        Refby : "K Rahul",
+        loc : "Pavoorchatram",
+        plcsts : "Not Started"
+
+    },
+    {
+        img :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLg7YYue_VyRsQLCwmguYP8nSLBwe24G8WgqJr8i_YxHwyHXbn9wqkZXAwdAvSGF9kVMk&usqp=CAU",
+        name : "Menan",
+        desc : "Fresher from madurai",
+        prmskills : "Html,Css,Javascript,React JS",
+        secskills: "Visual Studio Code, Git",
+        expsl : "2.5L" ,
+        edu : "B.Tech-EIE-2020-7.05 HSC - State Board - 2016 - 70%.SSLC - State Board - 2014 - 85%.",
+        avai : "Immediate",
+        isPlaced : True,
+        mbl : 936874590,
+        batch : "FSD05",
+        git : "https://github.com/vijaydev133",
+        gitUName: "vijaydev133" ,
+        Refby : "S Vinith",
+        loc : "Chennai",
+        plcsts : "Not Started"
+
+    }
+]
+
+let cards = document.querySelector(".cards")
+
+students.forEach(student =>{
+    
+})
